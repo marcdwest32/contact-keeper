1ab9861df3aee24381dc883ee1dbfa9c72d289f6
Marc <marcdwest@gmail.com>
2019-10-04 00:16:13 -0500

    successfully register user to db

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,20 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
 const Register = () => {
 	const alertContext = useContext(AlertContext);
+	const authContext = useContext(AuthContext);
 
 	const { setAlert } = alertContext;
+	const { register, error, clearErrors } = authContext;
+
+	useEffect(() => {
+		if (error === 'User already exists') {
+			setAlert(error, 'danger');
+			clearErrors();
+		}
+	}, [error]);
 
 	const [user, setUser] = useState({
 		name: '',
@@ -28,14 +38,18 @@ const Register = () => {
 		if (password !== password2) {
 			setAlert('Password Does Not Match', 'danger');
 		} else {
-			console.log('Register Submit');
+			register({
+				name,
+				email,
+				password,
+			});
 		}
 	};
 
 	return (
 		<div className='form-container'>
 			<h1>
-				Account <span className='text-primary'></span>
+				Account <span className='text-primary'>Register</span>
 			</h1>
 			<form onSubmit={onSubmit}>
 				<div className='form-group'>
