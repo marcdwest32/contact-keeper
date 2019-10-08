bee4ce83e60c3e89ab1faca4060f31a42f56fb68
Marc <marcdwest@gmail.com>
2019-10-07 23:16:11 -0500

    able to add contacts to database

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -6,6 +6,7 @@ import {
 	UPDATE_CONTACT,
 	FILTER_CONTACTS,
 	CLEAR_FILTER,
+	CONTACT_ERROR,
 } from '../types';
 
 export default (state, action) => {
@@ -19,14 +20,14 @@ export default (state, action) => {
 			return {
 				...state,
 				contacts: state.contacts.map(contact =>
-					contact.id === action.payload.id ? action.payload : contact
+					contact.id === action.payload._id ? action.payload : contact
 				),
 			};
 		case DELETE_CONTACT:
 			return {
 				...state,
 				contacts: state.contacts.filter(
-					contact => contact.id !== action.payload
+					contact => contact._id !== action.payload
 				),
 			};
 		case SET_CURRENT:
@@ -51,6 +52,11 @@ export default (state, action) => {
 			return {
 				...state,
 				filetered: null,
+			};
+		case CONTACT_ERROR:
+			return {
+				...state,
+				error: action.payload,
 			};
 		default:
 			return state;
