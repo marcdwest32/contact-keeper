4d097686980f91d8a705afb49d06d4e96a782e7f
Marc <marcdwest@gmail.com>
2019-10-11 12:00:35 -0500

    id to _id in UPDATE_CONTACT

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -29,7 +29,7 @@ export default (state, action) => {
 			return {
 				...state,
 				contacts: state.contacts.map(contact =>
-					contact.id === action.payload._id ? action.payload : contact
+					contact._id === action.payload._id ? action.payload : contact
 				),
 				loading: false,
 			};
@@ -47,7 +47,7 @@ export default (state, action) => {
 				contacts: [],
 				filtered: null,
 				error: null,
-				current: null,	
+				current: null,
 			};
 		case SET_CURRENT:
 			return {
